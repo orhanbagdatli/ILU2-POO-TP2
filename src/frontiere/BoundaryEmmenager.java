46a93ed5646fae476be6d01632c00f324334227a
orhanbagdatli <orhan.bagdatli@univ-tlse3.fr>
2025-11-07 13:53:34 +0100

    TP2

--- src/frontiere/BoundaryEmmenager.java
+++ src/frontiere/BoundaryEmmenager.java
@@ -27,7 +27,9 @@ public class BoundaryEmmenager {
 					break;
 
 				case 2:
-					//TODO a completer
+					System.out.println("Bienvenue villageois " + nomVisiteur);
+					int force = Clavier.entrerEntier("Quelle est votre force ?");
+					controlEmmenager.ajouterGaulois(nomVisiteur, force);
 					break;
 
 				default:
@@ -40,6 +42,17 @@ public class BoundaryEmmenager {
 	}
 
 	private void emmenagerDruide(String nomVisiteur) {
-		//TODO a completer
+		System.out.println("Bienvenue druide " + nomVisiteur);
+		int force = Clavier.entrerEntier("Quelle est votre force ?");
+		int effetPotionMax = -1;
+		int effetPotionMin = -1;
+		while (effetPotionMax<effetPotionMin) {
+			effetPotionMin = Clavier.entrerEntier("Quelle est la force de la potion plus faible que vous produisez ?");
+			effetPotionMax = Clavier.entrerEntier("Quelle est la force de la potion plus forte que vous produisez ?");
+			if (effetPotionMax<effetPotionMin) {
+				System.out.println("Attention druide, vous vous etes trompé entre le minimum et le maximum");			
+			}
+		}
+		controlEmmenager.ajouterDruide(nomVisiteur, force, effetPotionMin, effetPotionMax);
 	}
 }
